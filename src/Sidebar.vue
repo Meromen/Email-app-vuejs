eb80aa09a21d2a0c779d93b4c420f3c8c04f26cf
meromen <urabbbc@yandex.ru>
2018-12-18 15:48:13 +1000

    Complete project

--- src/Sidebar.vue
+++ src/Sidebar.vue
@@ -9,28 +9,32 @@
             </div>
         </div>
 
+        <div class="compose-wrapper">
+            <app-compose></app-compose>
+        </div>
+
         <ul class="inbox-nav">
             <li :class="{ active: activeView == 'app-inbox' }">
                 <a href="#" @click.prevent="navigate('app-inbox', 'Inbox')">
-                    <i class="fa fa-inbox"></i>Inbox <span class="label label-danger pull-right">?</span>
+                    <i class="fa fa-inbox"></i>Inbox <span class="label label-danger pull-right">{{ unreadMessages.length }}</span>
                 </a>
             </li>
 
             <li :class="{ active: activeView == 'app-sent' }">
                 <a href="#" @click.prevent="navigate('app-sent', 'Sent')">
-                    <i class="fa fa-envelope"></i>Sent <span class="label label-default pull-right">?</span>
+                    <i class="fa fa-envelope"></i>Sent <span class="label label-default pull-right">{{ sentMessages.length }}</span>
                 </a>
             </li>
 
             <li :class="{ active: activeView == 'app-important' }">
                 <a href="#" @click.prevent="navigate('app-important', 'Important')">
-                    <i class="fa fa-bookmark-o"></i>Important <span class="label label-warning pull-right">?</span>
+                    <i class="fa fa-bookmark-o"></i>Important <span class="label label-warning pull-right">{{ importantMessages.length }}</span>
                 </a>
             </li>
 
             <li :class="{ active: activeView == 'app-trash' }">
                 <a href="#" @click.prevent="navigate('app-trash', 'Trash')">
-                    <i class="fa fa-trash-o"></i>Trash <span class="label label-default pull-right">?</span>
+                    <i class="fa fa-trash-o"></i>Trash <span class="label label-default pull-right">{{ trashMessages.length }}</span>
                 </a>
             </li>
         </ul>
@@ -39,8 +43,15 @@
 
 <script>
     import {eventBus} from "./main";
+    import Compose from "./Compose.vue";
 
     export default {
+    	props:{
+    		messages: {
+    			type: Array,
+    			required: true
+    		}
+    	},
         created() {
             eventBus.$on('changeView', (data) => {
                 this.activeView = data.tag;
@@ -58,7 +69,31 @@
                     title: title
                 });
             }
+        },
+        computed: {
+        	unreadMessages() {
+        		return this.messages.filter(function(message) {
+        			return (message.type == 'incoming' && !message.isRead && !message.isDeleted);
+        		})
+        	},
+        	sentMessages(){
+        		return this.messages.filter(function(message) {
+        			return (message.type == 'outgoing' && !message.isDeleted);
+        		})
+        	},
+        	importantMessages(){
+        		return this.messages.filter(function(message) {
+        			return (message.type == 'incoming' && message.isImportant === true && !message.isDeleted);
+        		})
+        	},
+        	trashMessages(){
+        		return this.messages.filter(function(message) {
+        			return (message.isDeleted === true);
+        		})
+        	}
+        },
+        components: {
+        	appCompose: Compose
         }
-
     }
 </script>
